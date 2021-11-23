d2c444737b29155dfd56ff1172d216739351a88c
Flávio Malto <flaviomalto@gmail.com>
2021-11-23 11:13:16 -0300

    primeiro commit

---
+++ Jenkins-tum/src/main/java/com/malto/Jenkinstum/JenkinsTumApplication.java
@@ -0,0 +1,13 @@
+package com.malto.Jenkinstum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTumApplication.class, args);
+	}
+
+}
---
+++ Jenkins-tum/src/test/java/com/malto/Jenkinstum/JenkinsTumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malto.Jenkinstum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
